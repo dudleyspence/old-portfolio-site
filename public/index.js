42e94f286ce1e244ada4015a3dd401e0a5d9bedd
dudleyspence <dudleyspence5@gmail.com>
2024-08-08 00:31:36 +0200

    push up to go live on linkedin

--- public/index.js
+++ public/index.js
@@ -10,43 +10,6 @@ document
     menu.classList.toggle("open");
   });
 
-document
-  .getElementById("toggle-dark-light")
-  .addEventListener("click", function () {
-    // Toggle the dark mode class on the body
-    document.body.classList.toggle("dark-mode");
-
-    const logoImages = document.querySelectorAll(".website-logo");
-    const linkedin = document.getElementById("linkedinIcon");
-    const github = document.getElementById("githubIcon");
-    const githubproject = document.querySelectorAll("#projectGithubIcon");
-    const hamburger = document.getElementById("hamburger-icon-img");
-
-    const darklightimg = document.getElementById("toggle-dark-light-img");
-
-    if (document.body.classList.contains("dark-mode")) {
-      logoImages[0].src = "assets/header/DudleyLogo-dark-mode.png";
-      // logoImages[1].src = "assets/header/DudleyLogo-dark-mode.png";
-      linkedin.src = "assets/profile/linkedin-dark-mode.png";
-      github.src = "assets/profile/github-dark-mode.png";
-      hamburger.src = "./assets/header/Hamburger-icon-dark-mode.png";
-      darklightimg.src = "assets/header/sun.png";
-      githubproject.forEach((githubLogo) => {
-        githubLogo.src = "./assets/projects/github-dark-mode.png";
-      });
-    } else {
-      logoImages[0].src = "assets/header/DudleyLogo-light-mode.png";
-      // logoImages[1].src = "assets/header/DudleyLogo-light-mode.png";
-      linkedin.src = "assets/profile/linkedin-light-mode.png";
-      github.src = "assets/profile/github-light-mode.png";
-      hamburger.src = "assets/header/Hamburger-icon-light-mode.png";
-      darklightimg.src = "assets/header/moon.png";
-      githubproject.forEach((githubLogo) => {
-        githubLogo.src = "./assets/projects/github-light-mode.png";
-      });
-    }
-  });
-
 document.getElementById("toggle-dark-light").addEventListener("click", () => {
   // Check local storage and apply the theme on page load
   document.addEventListener("DOMContentLoaded", () => {
@@ -58,6 +21,41 @@ document.getElementById("toggle-dark-light").addEventListener("click", () => {
     }
   });
 });
+
+document
+  .getElementById("toggle-dark-light")
+  .addEventListener("click", function () {
+    // Toggle the dark mode class on the body
+    document.body.classList.toggle("dark-mode");
+
+    const logoImages = document.getElementById("website-logo");
+    const linkedin = document.getElementById("linkedinIcon");
+    const github = document.getElementById("githubIcon");
+    const githubproject = document.querySelectorAll("#projectGithubIcon");
+    const hamburger = document.getElementById("hamburger-icon-img");
+
+    const darklightimg = document.getElementById("toggle-dark-light-img");
+
+    if (document.body.classList.contains("dark-mode")) {
+      logoImages.src = "assets/header/DudleyLogo-dark-mode.png";
+      linkedin.src = "assets/profile/linkedin-dark-mode.png";
+      github.src = "assets/profile/github-dark-mode.png";
+      hamburger.src = "./assets/header/Hamburger-icon-dark-mode.png";
+      darklightimg.src = "assets/header/sun.png";
+      githubproject.forEach((githubLogo) => {
+        githubLogo.src = "./assets/projects/github-dark-mode.png";
+      });
+    } else {
+      logoImages.src = "assets/header/DudleyLogo-light-mode.png";
+      linkedin.src = "assets/profile/linkedin-light-mode.png";
+      github.src = "assets/profile/github-light-mode.png";
+      hamburger.src = "assets/header/Hamburger-icon-light-mode.png";
+      darklightimg.src = "assets/header/moon.png";
+      githubproject.forEach((githubLogo) => {
+        githubLogo.src = "./assets/projects/github-light-mode.png";
+      });
+    }
+  });
 
 window.onload = function () {
   // Reset the form fields when the page loads
